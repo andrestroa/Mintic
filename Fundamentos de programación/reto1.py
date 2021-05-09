89c3571198802c46491b143bf6040e370550e3a4
andrestroa <andrestroa@yahoo.com>
2021-05-08 20:11:03 -0500

    second commit changes in the negative numbers

--- Fundamentos de programación/reto1.py
+++ Fundamentos de programación/reto1.py
@@ -1,10 +1,17 @@
+from math import ceil #Importa función
+import os
+
+OLD_ANTENNAS = 18400 #Area que cumbre las antenas antiguas
+
+
 antennas ={  #Dicionario que contiene como llaves el tipo de antena y como valor su cobertura
     "a":35600,
     "b":6800,
     "c":59300,
     "d":24200,
     "e":7400,
-}
+}   
+
 
 
 def verification(area,nubs_of_antennas,types_of_atennas): #La función recibe 3 parametros
@@ -15,26 +22,43 @@ def verification(area,nubs_of_antennas,types_of_atennas): #La función recibe 3 
             print("Error en los datos ") #Si no se encuentra imprime el mensaje
         else: #Si la variable se encuentra se continua con el código
             for antena,valores in antennas.items(): #Ciclo for que recorre la llave y el valor del diccionario
-                if types_of_atennas == antena: #Si la variable types_of_atennas a alguna llave del diccionario
-                    print(round((area / valores))) #Se ejecuta la operación, redonde el resultado y lo imprime
+                if types_of_atennas == antena: #Si la variable types_of_atennas esta en alguna llave del diccionario
+                    new_atennas =(area-(nubs_of_antennas*OLD_ANTENNAS))/valores
+                    if new_atennas > 0:
+                        print(ceil(new_atennas))
+                    else:
+                        print(0)
+
+
+                    
     
 
 def run():
-    area = float(input('Ingrese el área de la zona de instalación m2: ')) #Solicita al usuario un número flotante
-    nubs_of_antennas = int(input('Ingrese el número de antenas que tiene ')) #Solicita al usuario un entero 
-
-    types_of_atennas = input("""Ingrese el tipo de la nueva antes que desea: 
+    os.system("cls")
     
+    try: #Try       
+        area = float(input('Ingrese el área de la zona de instalación m2: ')) #Solicita al usuario un número flotante
+        nubs_of_antennas = int(input('Ingrese el número de antenas que tiene ')) #Solicita al usuario un entero    
+        types_of_atennas = input("""Ingrese el tipo de la nueva antes que desea: 
+        
+        
+        a: 35600
+        b: 6800
+        c: 59300
+        d: 24200
+        e: 7400 
+        
+        """)                                                                        #Solicita al usuario que ingrese una string según las opciones del enunciado     
+        verification(area,nubs_of_antennas,types_of_atennas) #La función recibe los 3 parámetros anteriores
     
-    a: 35600
-    b: 6800
-    c: 59300
-    d: 24200
-    e: 7400 
-    
-    """)                                                                  #Solicita al usuario que ingrese una string según las opciones del enunciado     
-    verification(area,nubs_of_antennas,types_of_atennas) #La función recibe los 3 parámetros anteriores   
-    
-    
+    except:#Imprime mensjae si aparece error
+        print('Error en los datos')  
+         
+        
 if __name__ == '__main__':
     run()
+
+
+
+
+    
